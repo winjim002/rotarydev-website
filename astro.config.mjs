e1036dfdcbf0025cf6345231d2b6cf2b76694692
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2024-03-27 18:27:59 -0500

    chore: disable Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
@@ -101,5 +100,4 @@ export default defineConfig({
     clientPrerender: true,
     directRenderScript: true,
   },
-  adapter: vercelStatic(),
 });
